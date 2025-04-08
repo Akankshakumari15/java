281ada29e26c50617ec4e374f27ab60018690572
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-04-08 10:47:08 +0530

    hello

---
+++ conditions.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class conditions {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+      //if,else
+      //   int a = sc.nextInt();
+
+      //   if(a>=18){
+      //       System.out.println("adult");
+      // }
+      // else{
+      //   System.out.println("not adult");
+      // }
+
+      
+      //if else
+      // int button = sc.nextInt();
+
+      // if(button==1){
+      //   System.out.println("helllo");        
+      // }
+      // else if(button==2){
+      //   System.out.println("namaste");
+      // }
+      // else{
+      //   System.out.println("bonjour");
+      // }
+
+
+      //odd even
+      // int x = sc.nextInt();
+      // if(x%2==0){
+      //   System.out.println("it is an even number");
+      // }
+      // else{
+      //   System.out.println("it is an odd number");
+      // }
+
+
+      //switch
+      int button = sc.nextInt();
+      switch(button){
+
+      case 1:System.out.println("hello");
+             break;
+      case 2:System.out.println("namaste");
+             break;
+      case 3:System.out.println("bonjour");    
+             break;
+        default:System.out.println("invalid button");        
+      }
+    }
+}
