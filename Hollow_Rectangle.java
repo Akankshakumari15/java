6be94c5041402ab6a08787f46538b811865fdd77
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-04-14 08:14:11 +0530

    git.txt

--- Hollow_Rectangle.java
+++ Hollow_Rectangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solid_Rectangle {
+public class Hollow_Rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of rows:");
@@ -11,10 +11,15 @@ public class Solid_Rectangle {
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
+                if(i==1 || j==1 || i==n || j==m){
                 System.out.print("*");
             }
+            else{
+                System.out.print(" ");
+            }
+        }
             System.out.println();
         }
-        }
+    }
     
 }
--- Inverted_Half_Pyramid.java
+++ Inverted_Half_Pyramid.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class Solid_Rectangle {
+public class Inverted_Half_Pyramid {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of rows:");
         int n = sc.nextInt();
-        System.out.println("Enter no. of columns:");
-        int m = sc.nextInt();
         sc.close();
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
+            
             }
             System.out.println();
         }
-        }
+    }
     
 }
