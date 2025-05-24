09524b4500e04ff7132ac4cdcd44d546ed237fc6
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-05-24 09:43:32 +0530

    git.txt

---
+++ Collection_Framework.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Collection_Framework {
+    public static void main(String[] args) {
+        //EX 1
+    ArrayList<String> arr = new ArrayList<>();
+        System.out.println("String array: ");
+        //to add/assign elements into the array.
+         arr.add("akanksha");
+         arr.add("hi"); 
+         arr.add("byeee");
+         arr.add(0,"akuu");
+         System.out.println(arr);
+         Collections.sort(arr);System.out.println("After sorting:");
+         System.out.println(arr);
+          
+
+         //EX 2
+    ArrayList<Character> arr2 = new ArrayList<>();
+        System.out.println("Character array:");
+        //to add/assign elements into the array.
+         arr2.add('d');
+         arr2.add('e'); 
+         arr2.add('a');
+         arr2.add(0,'b');
+         System.out.println(arr2);
+         Collections.sort(arr2);System.out.println("After sorting:");
+         System.out.println(arr2);
+
+        //EX 3
+    ArrayList<Integer> arr1 = new ArrayList<>();
+    System.out.println("Integer array:");
+         arr1.add(178);
+         arr1.add(67); 
+         arr1.add(24);
+         arr1.add(0,298);
+         System.out.println(arr1);
+         
+         //to get a certain index element from an array
+         System.out.print("Getting element at index 2:");
+         System.out.println(arr1.get(2));
+         //to change an element existing in a array
+         System.out.print("Setting element at index 3(46):");
+         System.out.println(arr1.set(3, 46));
+         System.out.println(arr1);
+         //to remove an index element from the array
+         System.out.print("Removing element at index 1 from the array:");
+         System.out.println(arr1.remove(1));
+         System.out.println(arr1);
+         //to clear/delete the whole array
+         System.out.print("After clearing the array:");
+         arr1.clear();
+         System.out.println(arr1+" ");
+
+
+
+
+
+
+         
+
+         
+
+
+    }
+    
+}
--- Kadanes_algorithm.java
+++ Kadanes_algorithm.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
-
 public class Kadanes_algorithm {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of array:");   
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-        System.out.println("Enter elements in the array:");   
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
-        }
-        sc.close();
-       
-        for(int i=0;i<=arr.length;i++){
-              int sum=0; 
-            for(int j=i;j<arr.length;j++){
-              sum=sum+arr[j];
-            }      
-            System.out.println(sum);    
-        }
-    }
-       
         
+     try {
+      int [] arr={2,4,3,56,6,4,-2,-4};
+        int sum=0;int max =arr[0]; int a=0; 
+            for(int i=0;i<(arr.length-2);i++){
+            sum=arr[i]+arr[i+1]+arr[i+2];
+            if(sum>max){
+                max=sum;
+                a=i;
+              }
+            } 
+            System.out.println("The sum is:"+max);    
+             System.out.println("The elements that form the maximum subarray are:"+arr[a]+","+arr[a+1]+","+arr[a+2]);     
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("No maximum subarray found.");
+        }
+
     }
+          
 }
