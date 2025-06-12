c391dd5624e0fe6bf51a1f1f758cda1758144a9f
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-06-12 09:22:06 +0530

    git.txt

---
+++ Rotate_90Clockwise_matrix.java
@@ -0,0 +1,39 @@
+public class Rotate_90Clockwise_matrix {
+        public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        // Transpose the matrix 
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+         // Reverse each row
+        for (int i = 0; i < arr.length; i++) {
+            int left = 0;
+            int right = arr[0].length - 1;
+            while (left < right) {
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+         // Print the rotated matrix
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
