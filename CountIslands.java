46e822fa9fcd8d67dd29cff734d5c6b60f2426ac
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-06-27 19:44:13 +0530

    git.txt

---
+++ CountIslands.java
@@ -0,0 +1,41 @@
+public class CountIslands {
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        System.out.println("Number of islands: " + numIslands(grid));
+    }
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+        int Islandcount = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // If land is found, start DFS
+                if (grid[i][j] == '1') {
+                    Islandcount++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return Islandcount;
+    }
+    public static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        // Visit all 4 neighbors
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
+    }
+}
+
+
+
