98c48ae9f72e87eb78a1b22b034a06d12e58427e
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-07-01 20:34:10 +0530

    git.txt

--- BooleanMatrixProblem_SetTo1.java
+++ BooleanMatrixProblem_SetTo1.java
@@ -1,9 +1,9 @@
-public class BooleanMatrixProblem {
+public class BooleanMatrixProblem_SetTo1 {
     public static void main(String[] args) {
         int[][] mat = {
-            {1, 1, 1},
-            {1, 0, 1},
-            {1, 1, 1}
+            {1, 0, 0},
+            {0, 0, 0},
+            {0, 0, 1}
         };
         setZeroes(mat);
         for (int[] row : mat) {
@@ -12,32 +12,30 @@ public class BooleanMatrixProblem {
             System.out.println();
         }
     }
-
     static void setZeroes(int[][] mat) {
         int r = mat.length, c = mat[0].length;
         boolean[] row = new boolean[r];
         boolean[] col = new boolean[c];
-
-        // Mark rows and columns that have a 0
+        // rows and columns that have a 1
         for (int i = 0; i < r; i++)
             for (int j = 0; j < c; j++)
-                if (mat[i][j] == 0) {
+                if (mat[i][j] == 1) {
                     row[i] = true;
                     col[j] = true;
                 }
-
-        // Set entire row to 0
+        // Set entire row to 1 
         for (int i = 0; i < r; i++)
             if (row[i])
                 for (int j = 0; j < c; j++)
-                    mat[i][j] = 0;
-
-        // Set entire column to 0
+                    mat[i][j] = 1;
+        // Set entire column to 1 
         for (int j = 0; j < c; j++)
             if (col[j])
                 for (int i = 0; i < r; i++)
-                    mat[i][j] = 0;
+                    mat[i][j] = 1;
     }
 }
+
+
 
 
